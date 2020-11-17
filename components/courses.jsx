a1970d999d91dd1f94590a889c0d5426d95b51b1
Ravioli <atran160@mtroyal.ca>
2020-11-16 17:26:15 -0800

    Added features and fixed some css

--- components/courses.jsx
+++ components/courses.jsx
@@ -2,93 +2,6 @@ import React, { useState } from "react";
 import { Text, View, StyleSheet, Button, TextInput } from "react-native";
 import Constants from "expo-constants";
 
-// You can import from local files
-
-// or any pure javascript modules available in npm
-/*
-export default function Courses() {
-  const [course, setCourse] = React.useState();
-  const [id, setId] = React.useState();
-  const [name, setName] = React.useState();
-  const [description, setDescription] = React.useState();
-  const [changeId, setChangeId] = React.useState();
-  const [newDescription, setNewDescription] = React.useState();
-
-
-  const getCourses = () => {
-    const url = "https://excerise.uc.r.appspot.com/courses";
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => displayallCourses(data));
-  };
-
-  const displayallCourses = (data) => {
-    let allCourses = "";
-    data.forEach((course) => {
-      allCourses += course.course_id += course.course_name +=
-        course.description;
-      setCourse(allCourses);
-    });
-  };
-
-  const changeDescription = () => {};
-
-  const addCourse = () => {};
-
-  return (
-    <View style={styles.container}>
-    
-      <Button title="Get Courses" onPress={() => getCourses()} />
-      <Text>{course}</Text>
-      <Text>Course Id</Text>
-      <TextInput
-        style={styles.text}
-        onChangeText={(text) => setId(text)}
-        value={id}
-      />
-      
-      <Text>Course ID</Text>
-      <TextInput
-        style={styles.text}
-        onChangeText={(text) => setId(text)}
-        value={id}
-      />
-      <Text>Course Name</Text>
-      <TextInput
-        style={styles.text}
-        onChangeText={(text) => setName(text)}
-        value={name}
-      />
-      <Text>Description</Text>
-      <TextInput
-        style={styles.text}
-        onChangeText={(text) => setDescription(text)}
-        value={description}
-      />
-      <Button title="Add Courses" onPress={() => addCourses()} />
-      <Text>{id}</Text>
-      <Text>{name}</Text>
-      <Text>{description}</Text>
-      <Text>Course Id</Text>
-      <TextInput
-        style={styles.text}
-        onChangeText={(text) => setChangeId(text)}
-        value={changeId}
-      />
-      <Text>Description</Text>
-      <TextInput
-        style={styles.text}
-        onChangeText={(text) => setNewDescription(text)}
-        value={newDescription}
-      />
-      <Button title="Change Description" onPress={() => changeDescription()} />
-      <Text>{changeId}</Text>
-      <Text>{newDescription}</Text>
-    </View>
-  );
-}
-*/
-
 const addCourse = () => {
   const [id, setId] = React.useState("test1000");
   const [name, setName] = React.useState("testingprogram");
@@ -117,22 +30,6 @@ const addCourse = () => {
     });
   }
 
-  const getCourses = () => {
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://excerise.uc.r.appspot.com/courses";
-    fetch(proxy + url)
-      .then((resp) => resp.json())
-      .then((data) => displayallCourses(data));
-  };
-
-  const displayallCourses = (data) => {
-    let allCourses = "";
-    data.forEach((course) => {
-      allCourses += course.course_id += course.course_name +=
-        course.description;
-      setCourse(allCourses);
-    });
-  };
 
   return (
     <View style={styles.container}>
@@ -140,7 +37,7 @@ const addCourse = () => {
         style={{
           textAlign: "center",
           marginBottom: 15,
-          fontSize: 20,
+          fontSize: 30,
           fontWeight: "bolder",
         }}
       >
@@ -175,14 +72,18 @@ const addCourse = () => {
       <Button
         title="Add Course"
         onPress={() => {
-          insertData();
+          if (id == "" || name == "") {
+            alert("Course ID and course name have to be included")
+          } else {
+            insertData();
+            alert("Course added succesfully!")
+          }
         }}
       />
-      <Text>ID: {id}</Text>
-      <Text>Name: {name}</Text>
-      <Text>Description: {description} </Text>
-      <Button title="Get Courses" onPress={() => getCourses()} />
-      <Text>{course}</Text>
+      <Text style={{ marginTop: 10, marginBottom: 10, fontWeight: "bolder" }}>ID: {id}</Text>
+      <Text style={{ marginBottom: 10, fontWeight: "bolder" }}>Name: {name}</Text>
+      <Text style={{ marginBottom: 10, fontWeight: "bolder" }}>Description: {description} </Text>
+      
     </View>
   );
 };
@@ -190,13 +91,14 @@ const addCourse = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "20%",
     justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#ecf0f1",
     padding: 8,
   },
   text: {
     height: 40,
+    maxWidth: "100%",
     borderColor: "gray",
     borderWidth: 1,
     marginBottom: 10,
